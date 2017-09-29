356313ba59969be2e9fcd3bdc803e996cd027683
ArtOfCode <UP820915@uni.ds.port.ac.uk>
2017-09-29 10:51:12 +0100

    Rendering groundwork

--- webpages/replay.js
+++ webpages/replay.js
@@ -1,8 +1,29 @@
 let ws = new WebSocket("ws://" + window.location.hostname + ":" + (window.location.port || 80) + "/");
 let events = [];
 
+const renderEvent = evt => {
+  console.log('rendering', evt);
+};
+
 const runReplay = () => {
-  console.log(events);
+  if (events.length < 1) {
+    // There's nothing to simulate here. Returning allows the rest of the code to assume
+    // there's data in the array without doing the error checks every time.
+    return;
+  }
+
+  // Doing this gets us relative timestamps instead of absolute, which makes timing easier.
+  const firstEventTimestamp = events[0].timestamp;
+  events = events.map(x => Object.assign(x, { 'timestamp': x.timestamp - firstEventTimestamp }));
+
+  const simulationStart = Date.now();
+  events.forEach(x => {
+    const runAt = simulationStart + x.timestamp;
+    const millisUntilRun = runAt - Date.now();
+    setTimeout(() => {
+      renderEvent(x);
+    }, millisUntilRun);
+  });
 };
 
 // /replay.json gives us a list of all the mousemove events that the server holds.
