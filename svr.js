6b8da3749e9909287aef4dbbcfb87208a014b1a8
Rich Boakes <rjb@port.ac.uk>
2018-03-06 16:27:33 +0000

    Add lots of comments

--- svr.js
+++ svr.js
@@ -1,33 +1,57 @@
-'use strict';
+/// An excessively commented example.
 
-const http = require('http');
-const wsserver = require('ws').Server;
+// configuration
+const port = 8888;
+
+// libraries
 const express = require('express');
+const http = require('http');
+const ws = require('ws');
+const ip = require("ip");
 
-const server = http.createServer();
-const wss = new wsserver({ server: server });
+// create an express application
+// http://expressjs.com/en/api.html
 const app = express();
 
-server.on('request', app);
+// create an http server that uses express for handling requests
+// https://nodejs.org/api/http.html
+const server = http.createServer(app);
 
-function ws_broadcast(message) {
-    wss.clients.forEach( (client) => {
-      if(client.readyState === client.OPEN){
-        try {
-          client.send(message);
-        } catch (e) {
+// Create a WebSocket Server and connect it to the http
+// https://github.com/websockets/ws/blob/master/doc/ws.md
+const wss = new ws.Server({ server: server });
+
+// when a message is received, use the wss.clients
+// list to loop over all connected clients, and
+// if their connection is still open, send them a
+// copy of the message.
+function messageHandler(message) {
+    wss.clients.forEach((client) => {
+        if(client.readyState === client.OPEN) {
+            try {
+                client.send(message);
+            } catch (err) {
+              // errors are ignored
+            }
         }
-      }
     });
 }
 
-function ws_responder(ws) {
-	ws.on( 'message', ws_broadcast );
+// for any messages that arrive via the ws
+// web socket, invoke the messageHandler
+function connectionHandler(ws) {
+  ws.on('message', messageHandler);
 }
 
+// When a new connection is received
+// invoke the connection handler
+wss.on('connection', connectionHandler);
 
-app.use(express.static(__dirname + '/webpages', { extensions: ['html', 'css', 'js'] }));
+// serve the client using
+// express's default static middleware
+app.use( express.static(`${__dirname }/webpages`) );
 
-wss.on('connection', ws_responder);
-
-server.listen(8080, function () { console.log('Server started.'); });
+// start the server
+server.listen(port, () => {
+    console.log('Server started:', `http://${ip.address()}:${port}` )
+});
